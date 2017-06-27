6b2f64408680d0119245871f1cb772987d9feeee
Nick Farrow <VeggieVampire@users.noreply.github.com>
2017-06-27 05:35:09 -0500

    Create fail2banlog.sh

---
+++ fail2banlog.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#  *****Z
+#
+# fail2ban status checking script
+#
+# Author Nick Farrow
+#
+# Initial Release: 0
+
+
+log='/var/log/fail2ban.log'
+
+# Simple Reports
+        Simple_ReportZ()
+        {
+		echo "Generating Simple Reports"
+	awk '($(NF-1) = /Ban/){print $NF}' $log | sort | uniq -c | sort -n
+		printf "\n"
+        }
+
+#problematic subnets
+        problematic_subnetsZ()
+        {
+		echo "problematic subnets Reports"
+		echo "ctrl+c to exit"
+	zgrep -h "Ban " $log* | awk '{print $NF}' | awk -F\. '{print $1"."$2"."}' | sort | uniq -c  | sort -n | tail
+		printf "\n"
+        }
+
+#access_exploits
+        access_exploitZ()
+        {
+		echo "This shows us which services each IP address has been trying to access_exploit"
+	grep "Ban " $log | awk -F[\ \:] '{print $10,$8}' | sort | uniq -c | sort -n
+		printf "\n"
+        }
+
+#todays activity
+        todays_activityZ()
+        {
+		echo "This generate a summary of the days activity"
+	grep "Ban " $log | grep `date +%Y-%m-%d` | awk '{print $NF}' | sort | awk '{print $1,"("$1")"}' | nslookup | uniq -c | sort -n
+		printf "\n"
+        }
+		
+# MAIN BODY OF SCRIPT
+#
+# Present the menu selection to the user
+
+        echo " What would you like to do?"
+        PS3="Please select a numeric option:  "
+
+        select option in Simple_Reports problematic_subnets access_exploits todays_activity	Exit
+
+        do 
+                case "$option"
+                        in
+                                Simple_Reports    )  Simple_ReportZ;;
+								problematic_subnets    )  problematic_subnetsZ;;
+								access_exploits    )  access_exploitZ;;
+								todays_activity    )  todays_activityZ;;
+                                Exit         )  exit 0;;
+                                *            )  echo "Invalid selection ! ";;
+                       esac
+       done
+
